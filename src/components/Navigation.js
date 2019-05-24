7478751e4b899176776f8f7fb80c86749b688339
Antonina Druzhkina <antonina.druzhkina@isolutions.ch>
2019-05-24 17:57:55 +0300

    add link to catalog on navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Navigation extends Component {
     render() {
         return (
             <div className="navigation">
                 Navigation
+                <Link to="/catalog" className="catalog-button">Catalog</Link>
             </div>
         );
     }
