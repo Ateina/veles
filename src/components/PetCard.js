e708ca2a30465b94d1fbf7b02253a47b9535a7cf
Ateina <totora246@gmail.com>
2019-05-26 02:29:57 +0300

    update pet card with name

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Petcard extends Component {
+class PetCard extends Component {
+    static propTypes = {
+        pet: PropTypes.object
+    }
+
     render() { 
         return (
-            <div className="pet-card">Pet card</div>
+            <div className="pet-card">
+                Name: {this.props.pet.name}
+            </div>
         );
     }
 }
+
+
  
-export default Petcard;
+export default PetCard;
