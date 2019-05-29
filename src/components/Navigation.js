eb8ce280c6a51c4223850f503e6bfe19d42fad3a
Antonina Druzhkina <antonina.druzhkina@isolutions.ch>
2019-05-29 17:15:57 +0300

    update some components to stateless mode;

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 
-class Navigation extends Component {
-    render() {
-        return (
-            <nav className="navigation">
-                <ol className="navigation-links">
-                    <li className="navigation-link">
-                        <Link to="/">
-                            <div className="logo">Veles Home</div>
-                        </Link>
-                    </li>
-                    <li className="navigation-link">
-                        <Link to="/catalog">
-                            <div>Catalog</div>
-                        </Link>
-                    </li>
-                    <li className="navigation-link">
-                        <Link to="/login" >
-                            <div className="login-link">Login</div>
-                        </Link>
-                    </li>
-                </ol>
-            </nav>
-        );
-    }
+function Navigation() {
+    return (
+        <nav className="navigation">
+            <ol className="navigation-links">
+                <li className="navigation-link">
+                    <Link to="/">
+                        <div className="logo">Veles Home</div>
+                    </Link>
+                </li>
+                <li className="navigation-link">
+                    <Link to="/catalog">
+                        <div>Catalog</div>
+                    </Link>
+                </li>
+                <li className="navigation-link">
+                    <Link to="/login" >
+                        <div className="login-link">Login</div>
+                    </Link>
+                </li>
+            </ol>
+        </nav>
+    );
 }
 
 export default Navigation;
--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class PetCard extends Component {
-    static propTypes = {
-        pet: PropTypes.object
-    }
-
-    render() {
-        return (
-            <div className="pet-card">
-                <img src={this.props.pet.image ? this.props.pet.image : 'images/no-photo.jpg'} alt="pet"/>
-                <span className="name">{this.props.pet.name}</span>
-            </div>
-        );
-    }
+PetCard.propTypes = {
+    pet: PropTypes.object
 }
 
-
+function PetCard({pet}) {
+    return (
+        <div className="pet-card">
+            <img src={pet.image ? pet.image : 'images/no-photo.jpg'} alt="pet"/>
+            <span className="name">{pet.name}</span>
+        </div>
+    );
+}
 
 export default PetCard;
