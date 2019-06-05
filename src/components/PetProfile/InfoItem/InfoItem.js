aa82d3f1fb37a382d31d9eee60cdc737c70c2230
Antonina Druzhkina <antonina.druzhkina@isolutions.ch>
2019-06-05 20:03:29 +0300

    style InfoItem;

--- src/components/PetProfile/InfoItem/InfoItem.js
+++ src/components/PetProfile/InfoItem/InfoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function InfoItem(props) {
     return (
         <div className="info-item">
-            <div className="label">{props.label}</div>
+            <div className="label">{props.label} : </div>
             <div className="value">{props.value}</div>
         </div>
     );
