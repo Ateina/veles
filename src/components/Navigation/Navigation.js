e93871d668868433c8579de7de799a42ed104fd5
Grenzen <grenzentur@rambler.ru>
2019-06-22 17:38:22 +0300

    css-style: add navigation styling, transition from desktop to mobile and some fonts

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,25 +5,35 @@ import './styles.css'
 
 function Navigation() {
     return (
-        <nav className="navigation">
-            <ol className="navigation-links">
-                <li className="navigation-link">
-                    <NavLink to="/">
-                        <div className="logo">Veles Home</div>
-                    </NavLink>
-                </li>
-                <li className="navigation-link">
-                    <NavLink to="/catalog">
-                        <div>Catalog</div>
-                    </NavLink>
-                </li>
-                <li className="navigation-link">
-                    <NavLink to="/login" >
-                        <div className="login-link">Login</div>
-                    </NavLink>
-                </li>
-            </ol>
-        </nav>
+       
+        <React.Fragment>
+            <input type="checkbox" id="nav-toggle" hidden></input>
+        
+
+            <nav className="navigation">
+
+                <label htmlFor="nav-toggle" className="nav-toggle" onСlick>
+                </label>
+
+                <ol className="navigation-links">
+                    <li className="navigation-link">
+                        <NavLink to="/">
+                            <div className="logo">Veles Home</div>
+                        </NavLink>
+                    </li>
+                    <li className="navigation-link">
+                        <NavLink to="/catalog">
+                            <div>Catalog</div>
+                        </NavLink>
+                    </li>
+                    <li className="navigation-link">
+                        <NavLink to="/login" >
+                            <div className="login-link">Login</div>
+                        </NavLink>
+                    </li>
+                </ol>
+            </nav>
+        </React.Fragment>
     );
 }
 
