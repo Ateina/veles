2dc1922020c2e49e0ea8d45d9f0623db7f1f6a9e
Ateina <totora246@gmail.com>
2019-11-09 00:11:43 +0300

    add nre pet form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import MainPage from './MainPage/MainPage';
 import Catalog from './Catalog/Catalog';
 import NotFound from './NotFound/NotFound';
@@ -7,13 +8,15 @@ import Navigation from './Navigation/Navigation';
 import Login from './Login/Login';
 import Footer from './Footer/Footer';
 import PetProfile from './PetProfile/PetProfile';
+import NewPetForm from './Forms/NewPetForm/NewPetForm';
 
 const App = () => (
     <BrowserRouter>
         <Navigation />
         <Switch>
             <Route exact path="/" component={MainPage} />
-            <Route path="/catalog" component={Catalog} />
+            <Route exact path="/catalog" component={Catalog} />
+            <Route path="/catalog/new-pet" component={NewPetForm} />
             <Route path="/login" component={Login}/>
             <Route path="/petprofile/:petId" component={PetProfile} />
             <Route component={NotFound} />
